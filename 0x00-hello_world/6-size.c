2cc0ce566cc3786af040a4aaa00ebee5278e1417
Akande John <projectprimeaj@gmail.com>
2022-06-30 15:41:06 -0700

    task7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the size of variuos types
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int a;
-long int b;
-long long int e;
-float d;
-char c;
-	    
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", sizeof(l);
+printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
 printf("Size of a float: %lu byte(s)\n", sizeof(d));
 return (0);
