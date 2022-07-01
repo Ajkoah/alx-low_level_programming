2787645dd21a8edc42ea46bfc53b9382f4dcb18a
Akande John <projectprimeaj@gmail.com>
2022-07-01 16:06:54 -0700

    task1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -28,6 +28,6 @@ int main(void)
 		else
 			printf("%d is zero\n", n)
 
-		return (0);
+		return 0;
 
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,35 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
- *main - Entry point
- *Description: 
+ *main - Print the last digt of number stored
+ * in variable n.
+ *Return (0)
  */
 
 int main(void)
 {
-int n;
-srand(time(0));
+	int n;
+        char last[] = "Last digit of";
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-n = rand() - RAND_MAX / 2;
-
-					/* your code goes there */
-return (0);
+	/* your code goes here*/
+	printf("%s %dis %d is ", last, n, n % 10);
+	if (n % 10 > 5)
+	{
+	printf("greater than 5\n")
+	}
+	else if (n % 10 == 0)
+	{
+	printf("0\n")
+	}
+	else
+	{
+	printf("less than 6 and not 0\n")
+	}       	
+	return (0);
 
 }
