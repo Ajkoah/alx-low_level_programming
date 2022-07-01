39a6ec31a156b2d7bdd9a4fa702ddcae6cee8201
Akande John <projectprimeaj@gmail.com>
2022-07-01 14:33:27 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ n = rand() - RAND_MAX / 2;
 if (n>0) {
 	printf("is positive\n");
 }
-else if (n==0) {
+if (n==0) {
 	printf("is zero\n");
 }
-else (n<0) {
+if (n<0) {
 	printf("is negative\n");
 }
 return (0);
