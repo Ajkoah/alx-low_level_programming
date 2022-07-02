03344938545d3c0a52a6a82c33adc4b388172739
Akande John <projectprimeaj@gmail.com>
2022-07-01 18:19:21 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- Entry point 
+ * Description print all single digits of base 10 from 0
+ * Return: 0 successs
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
