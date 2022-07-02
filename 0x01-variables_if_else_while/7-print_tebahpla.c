1bba37a24d59f1812237a70a1237d480ad8f305e
Akande John <projectprimeaj@gmail.com>
2022-07-01 18:37:30 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- Entry point
+ * Description: alphabets in reverse
+ * Return: 0 success
+ */
+int main(void)
+{
+	char letter ='z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
