1aa3edbdee5f6e5624bc2dfb5a4fa213b528bdb8
Akande John <projectprimeaj@gmail.com>
2022-07-01 17:59:33 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- Entry point
+ * Description: print lower then upper case alphabets
+ * Return: 0 success
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while(letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
+
