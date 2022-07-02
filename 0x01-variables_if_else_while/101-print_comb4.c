7729e162764d439abb8eddf00daa3ca442489d5c
Akande John <projectprimeaj@gmail.com>
2022-07-01 21:14:23 -0700

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j ='0'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
@@ -33,4 +33,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}							
+}						
