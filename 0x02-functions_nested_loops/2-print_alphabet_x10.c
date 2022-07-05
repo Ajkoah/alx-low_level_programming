c582097cb9cf1758711ba7b6fccbb5b67fce90ba
Akande John <projectprimeaj@gmail.com>
2022-07-05 12:03:36 -0700

    3tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- check the code
+ * 
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main- Check the code
+ * 
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
