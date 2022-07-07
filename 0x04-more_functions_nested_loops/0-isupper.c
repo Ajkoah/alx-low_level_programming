2a6fee91e93ec3b64516dfa990ee01b010dc76e1
Akande John <projectprimeaj@gmail.com>
2022-07-07 09:07:12 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,11 +8,5 @@
 
 int _isupper(int c)
 {
-	char c;
-	
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
