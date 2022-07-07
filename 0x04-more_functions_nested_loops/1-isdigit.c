5ec1eb0f500e13e2957e728eeb8e9cbe2ca94959
Akande John <projectprimeaj@gmail.com>
2022-07-07 09:55:41 -0700

    task 1 to 3

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - function to check for a digit
+ * @c: int to be checked
+ * Return: AAlways 0
+ */
+int _isdigit(int c);
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied
+ * @b: int to be multi plied
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int c;
+	 
+	c = a * b;
+	return (c);
+}
