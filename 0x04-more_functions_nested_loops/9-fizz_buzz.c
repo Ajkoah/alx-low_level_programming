194e03da85399135d72328fc74740a611161120a
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-07 22:10:23 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,15 +17,15 @@ int main(void)
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf(" Buzz")
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz")
+			printf(" FizzBuzz");
 		}
 		else if (i == 1)
 		{
-			printf("%d", i)
+			printf("%d", i);
 		}
 		else
 		{
