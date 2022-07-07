1b3d73c29838ad04c87a0d5945b3b1e8713c0642
Akande John <projectprimeaj@gmail.com>
2022-07-07 15:04:25 -0700

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -29,7 +29,4 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
-}
+			printf("Remaining days: %d\n", 365 - day);}
