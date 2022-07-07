facb15b4802afbca939bd21a286a6bdbc45e83ef
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-07 22:26:58 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,21 +4,3 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-int largest_number(int a, int b, int c)
-{
-	int largest;
-	
-	if (a >= b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-	return (largest);
-}
