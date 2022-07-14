f0fe6ab48af6a68abb447eb3d715887e3ad63be9
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-14 23:16:28 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,7 +15,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
-	
+
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
@@ -24,7 +24,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		tens = num / 10;
 	}
 	for (; *n1; n1--, r_index--)
-	{	
+	{
 		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
