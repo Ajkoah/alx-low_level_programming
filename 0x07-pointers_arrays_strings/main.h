ef9f67f1da1be673555763b365c6413a8c1249a4
Akande John <projectprimeaj@gmail.com>
2022-07-18 15:40:33 -0700

    test files and correction

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 int _putchar(char c);
-void *_memset(void *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
