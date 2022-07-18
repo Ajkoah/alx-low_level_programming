9a83e19fc2dfbb2c82e933241ce7f71a8e6919d3
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-19 00:09:20 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_diagsums- prints the sum of the two diagonals of a square matrix of integers.
+ * print_diagsums- prints the sum of the two diagonals of
+ * a square matrix of integers.
  * @a: matrix of integers
  * @size: size of the matrix
  */
