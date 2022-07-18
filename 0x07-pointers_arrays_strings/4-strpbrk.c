c4ac058d27d5cfdb82dbcda15503324f96bd7330
Akande John <projectprimeaj@gmail.com>
2022-07-18 15:49:14 -0700

    corrections

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,18 +7,18 @@
  * Return: If a set is matched - a pointer to the matched byte.
  * If no set is matched - NULL.
  */
- char *_strpbrk(char *s, char *accept)
- {
-    int index;
-    
-    while (*s)
-    {
-          for (index = 0; accept[index]; index++)
-          {
-                if (*s == accept[index])
-                          return (s);
-            }
-            s++;
-      }
-      return ('\0');
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
 }
