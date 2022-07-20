50085a8734376602c876ffcfa2a148fd1dde153c
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-20 19:37:27 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -19,8 +19,22 @@ int find_strlen(char *s)
 		len += find_strlen(s + len);
 	}
 	return (len);
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ * Return: If the string is a palindrome - 1.
+ * If the string is not a palindrome - 0.
+ */
+ int check_palindrome(char *s, int len, int index)
+ {
+ 	if (s[index] == s[len / 2])
+		return (1);
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+	return (0);
 }
-
 /**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
