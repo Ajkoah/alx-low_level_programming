ef7757820cb09ca80694fbd5db49d67bb95456e7
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-22 21:15:25 +0100

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -15,7 +15,8 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
-		else if (num > 0)break;
+		else if (num > 0)
+			break;
 	} while (*s++);
 	return (num * sign);
 }
