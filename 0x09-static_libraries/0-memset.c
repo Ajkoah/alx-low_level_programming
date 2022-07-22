a79f1e77cb5e6e6d1de78ffd414c4417e46f76d0
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-22 21:09:53 +0100

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * _memset - function that fills memory with a constant byte.
- * @s: pointer to the memory area 
+ * @s: pointer to the memory area
  * @b: constant byte
  * @n: number of bytes to be filled
  * Return: A pointer to the filled memory area @s
@@ -10,9 +10,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
-	
+
 	for (a = 0; a < n; a++)
-	{						
+	{					
 		s[a] = b;
 	}
 	return (s);
