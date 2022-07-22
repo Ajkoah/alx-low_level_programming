2cdba6f29f4c138a1d7226f0be489892b788c2d1
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-22 21:12:37 +0100

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * _memcpy- function that copies memory are
  * @dest: memory are to be copied to
  * @src: memory area to be copied from
@@ -10,7 +10,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
-	
+
 	for (a = 0; a < n; a++)
 	{
 		dest[a] = src[a];
