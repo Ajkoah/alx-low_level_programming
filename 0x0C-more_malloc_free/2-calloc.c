a510c7506c8dfb0519c9e5bde47d4f8e8b8d52b4
Ajkoah <106863370+Ajkoah@users.noreply.github.com>
2022-07-29 00:11:04 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,12 +3,11 @@
 
 /**
  * _calloc - Allocates memory for an array of a certain number
- *           of elements each of an inputted byte size.
+ * of elements each of an inputted byte size.
  * @nmemb: The number of elements.
  * @size: The byte size of each array element.
- *
  * Return: If nmemb = 0, size = 0, or the function fails - NULL.
- *         Otherwise - a pointer to the allocated memory.
+ * Otherwise - a pointer to the allocated memory.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
